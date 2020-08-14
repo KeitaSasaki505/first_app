84c7bef95123d0e4bd4ee3df575c3169a432da95
KeitaSasaki505 <cutba505@gmail.com>
2020-08-14 19:18:32 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new　
+  end  
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
